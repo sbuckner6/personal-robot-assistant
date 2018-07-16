31af6971e44aa25dcbcbf2b8f18466256dc484bf
Simon <sbbucknerv@gmail.com>
2018-07-16 18:19:04 -0400

    added caching check for non-docker builds

--- app/access.py
+++ app/access.py
@@ -14,7 +14,13 @@ class HoundifyAccessObject:
                 requestInfo={}
             )
 
-            self.redis = Redis(host='redis', port=6379, db=0)
+            self.caching_enabled = False
+
+            try:
+                self.redis = Redis(host='redis', port=6379, db=0)
+                self.caching_enabled = True
+            except:
+                print("Unable to connect to Redis instance!")
 
     instance = None
 
@@ -22,22 +28,27 @@ class HoundifyAccessObject:
         if not HoundifyAccessObject.instance:
             HoundifyAccessObject.instance = HoundifyAccessObject.__instance()
         else:
-            HoundifyAccessObject.instance.text_client = houndify.TextHoundClient(
-                clientID=Config.Houndify.CLIENT_ID,
-                clientKey=Config.Houndify.CLIENT_KEY,
-                userID=Config.Houndify.USER_ID,
-                requestInfo={}
-            )
-
-            HoundifyAccessObject.instance.redis = Redis(host='redis', port=6379, db=0)
+            print("Attempting to reinstantiate singleton")
+            # HoundifyAccessObject.instance.text_client = houndify.TextHoundClient(
+            #     clientID=Config.Houndify.CLIENT_ID,
+            #     clientKey=Config.Houndify.CLIENT_KEY,
+            #     userID=Config.Houndify.USER_ID,
+            #     requestInfo={}
+            # )
+            #
+            # HoundifyAccessObject.instance.redis = Redis(host='redis', port=6379, db=0)
 
     def query(self, querytext):
-        answer = HoundifyAccessObject.instance.redis.get(querytext)
+        answer = None
+        caching = HoundifyAccessObject.instance.caching_enabled
+        if caching:
+            HoundifyAccessObject.instance.redis.get(querytext)
 
-        if answer is None:
+        if not answer:
             response = HoundifyAccessObject.instance.text_client.query(querytext)
             answer = response['AllResults'][0]['SpokenResponseLong']
-            HoundifyAccessObject.instance.redis.set(querytext, answer)
+            if caching:
+                HoundifyAccessObject.instance.redis.set(querytext, answer)
         else:
             answer = answer.decode('UTF-8')
 
